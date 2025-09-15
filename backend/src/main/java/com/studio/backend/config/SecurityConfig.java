ac97f47f9046754d44d3f3ff6d3818314e5f9dab
Maheesh09 <pramudithamaheesha@gmail.com>
2025-09-15 16:55:49 +0530

    resolve

--- backend/src/main/java/com/studio/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/studio/backend/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/customers/register", "/customer", "/customers").permitAll()
+                        .requestMatchers("http://localhost:8080/customer", "/customer", "/customers").permitAll()
                         .anyRequest().authenticated())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()));
         return http.build();
