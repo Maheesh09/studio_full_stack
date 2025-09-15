6a900394d61fe6e36aa773e7234aabf1d3e86217
Maheesh09 <pramudithamaheesha@gmail.com>
2025-09-16 00:48:42 +0530

    Revert "WIP: local edits before switching to Maheesh"
    This reverts commit 0f01361e664c323771835111d7e2157b5e234899.

---
+++ backend/src/main/java/com/studio/backend/Service/CustomerService.java
@@ -0,0 +1,46 @@
+package com.studio.backend.Service;
+import com.studio.backend.dto.CustomerLoginRequest;
+import com.studio.backend.dto.CustomerLoginResponse;
+import com.studio.backend.dto.CustomerRegistrationRequest;
+import com.studio.backend.exception.EmailAlreadyUsedException;
+import com.studio.backend.exception.InvalidCredentialException;
+import com.studio.backend.model.Customer;
+import com.studio.backend.repository.CustomerRepository;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CustomerService {
+    private final CustomerRepository repo;
+    private final PasswordEncoder encoder;
+
+    public CustomerService(CustomerRepository repo, PasswordEncoder encoder) {
+        this.repo = repo;
+        this.encoder = encoder;
+    }
+
+    @Transactional
+    public Integer register(CustomerRegistrationRequest req){
+        if(repo.existsByEmail(req.email())){
+            throw new EmailAlreadyUsedException();
+        }
+        Customer c = new Customer();
+        c.setCustomer_name(req.name());
+        c.setCustomer_phone(req.phone());
+        c.setCustomer_email(req.email());
+        c.setCustomer_password(encoder.encode(req.password()));
+        return  repo.save(c).getCustomer_id();
+    }
+    public CustomerLoginResponse login(CustomerLoginRequest req){
+        Customer c = repo.findByEmail(req.email())
+                .orElseThrow(InvalidCredentialException::new);
+
+        if(!encoder.matches(req.password(),c.getCustomer_password())){
+            throw new InvalidCredentialException();
+        }
+        return new CustomerLoginResponse("ok",c.getCustomer_id(),c.getCustomer_name(),c.getCustomer_email());
+    }
+}
+
