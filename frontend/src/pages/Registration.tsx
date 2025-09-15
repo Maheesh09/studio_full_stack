56c461d819d714b40e2dbb9733e8c491b1767cb3
Maheesh09 <pramudithamaheesha@gmail.com>
2025-09-15 16:47:25 +0530

    re

--- frontend/src/pages/Registration.tsx
+++ frontend/src/pages/Registration.tsx
@@ -69,7 +69,7 @@ setIsSubmitting(true);
 
 
 try {
-const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/register`, {
+const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/customer`, {
 method: "POST",
 headers: { "Content-Type": "application/json" },
 body: JSON.stringify({
